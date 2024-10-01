0f11081455bed861be67f910ec7825a9504b372f
Dave.Potter <dave@datonalabs.org>
2024-10-01 10:54:34 +0100

    refactor CountVotes

--- ballot/functions/CountVotes.js
+++ ballot/functions/CountVotes.js
@@ -1,6 +1,72 @@
 const { sha256 } = await import('npm:@noble/hashes/sha2');
 const secp = await import('npm:@noble/secp256k1');
 
+class BubbleRPCManager {
+
+  privateKey;
+  nextId = 0;
+
+  constructor(privateKey) {
+    this.privateKey = privateKey;
+  }
+
+  async read(contentId, responseType='text', options) {
+    return this._send(contentId.provider, responseType, await this._constructRequest(contentId, 'read', {}, options));
+  }
+
+  async _constructRequest(contentId, method, params={}, options) {
+    const request = {
+      jsonrpc: "2.0",
+      id: this.nextId++,
+      method: method,
+      params: {
+          timestamp: Date.now(),
+          nonce: crypto.randomUUID(),
+          chainId: contentId.chain,
+          contract: contentId.contract,
+          file: contentId.file,
+          ...params,
+          options
+      }
+    }
+    return this._sign(request);
+  }
+
+  async _sign(request) {
+    if (request.options === undefined) delete request.options;
+    const hash = sha256(JSON.stringify(request));
+    const sig = await secp.signAsync(hash, this.privateKey);
+    request.params.signature = sig.toCompactHex() + secp.etc.bytesToHex([27+sig.recovery]);
+    return request;
+  }
+
+  async _send(provider, responseType, signedRequest) {
+    const httpRequest = {
+      url: provider,
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      data: signedRequest,
+      responseType: responseType
+    }
+    const apiResponse = await Functions.makeHttpRequest(httpRequest);
+    if (apiResponse.error) throw Error("Request failed: ("+apiResponse.code+") "+apiResponse.message);
+    else if (apiResponse.data) {
+        const {error, result} = JSON.parse(apiResponse.data);
+        if (error) throw Error("Bubble Error: ("+error.code+") "+error.message);
+        return result;
+    }
+    else {
+      throw Error("Unexpected API response: "+JSON.stringify(apiResponse));
+    }
+  }
+
+}
+
+
+//
+// Main
+//
+
 const privateKey = args[0];
 
 const contentId = {
@@ -10,47 +76,9 @@ const contentId = {
     file: "0x0000000000000000000000000000000000000000000000000000000000000001"
 }
 
-async function constructReadRequest(contentId, options) {
-    const request = {
-        jsonrpc: "2.0",
-        id: 1,
-        method: 'read',
-        params: {
-            timestamp: Date.now(),
-            nonce: crypto.randomUUID(),
-            chainId: contentId.chain,
-            contract: contentId.contract,
-            file: contentId.file
-        }
-    }
+const bubbleManager = new BubbleRPCManager(privateKey);
 
-    if (request.options === undefined) delete request.options;
-    const hash = sha256(JSON.stringify(request));
-    const sig = await secp.signAsync(hash, privateKey);
-    request.params.signature = sig.toCompactHex() + secp.etc.bytesToHex([27+sig.recovery]);
-    return request;
-}
+const result = await bubbleManager.read(contentId, 'text');
 
-const request = await constructReadRequest(contentId);
+return Functions.encodeString(result);
 
-const httpRequest = {
-    url: contentId.provider,
-    headers: { 
-        'Content-Type': 'application/json'
-    },
-    method: 'POST',
-    data: request,
-    responseType: 'text'
-}
-
-const apiResponse = await Functions.makeHttpRequest(httpRequest);
-
-if (apiResponse.error) throw Error("Request failed: ("+apiResponse.code+") "+apiResponse.message);
-
-if (apiResponse.data) {
-    const {error, result} = JSON.parse(apiResponse.data);
-    if (error) throw Error("Bubble Error: ("+error.code+") "+error.message);
-    return Functions.encodeString(result);
-}
-
-return Functions.encodeString(JSON.stringify(apiResponse).slice(0,255));
